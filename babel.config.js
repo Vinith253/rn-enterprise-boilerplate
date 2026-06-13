a05db7ed96b35bf82ee6556d09754169a8096746
Vinith S <vinith1496@gmail.com>
2026-06-13 10:53:30 +0530

    feat(architecture): setup enterprise folder structure

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@app': './src/app',
+          '@features': './src/features',
+          '@services': './src/services',
+          '@shared': './src/shared',
+          '@hooks': './src/hooks',
+          '@utils': './src/utils',
+          '@theme': './src/theme',
+          '@constants': './src/constants',
+          '@types': './src/types',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
